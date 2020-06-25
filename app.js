8eeaa08fd6e84a2a795d858f0dd571f3e6a90803
Abdellatif Beldjelti <a.beldjelti@esi-sba.dz>
2020-06-25 19:49:59 +0100

    Adding the "Add" and "Delete" functions

--- app.js
+++ app.js
@@ -22,6 +22,48 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+//users table
+users=[];
+
+//show users
+app.get("/utilisateurs",function(req,res){
+  res.render("index",{users:users});
+});
+
+//add user function
+app.post("/add",function(req,res){
+  users.push({id:req.body.id,name:req.body.name,pw:req.body.pw});
+  res.redirect("/utilisateurs");
+});
+
+//modify user function
+app.post("/modify",function(req,res){
+  var name=req.body.name;
+  var pw=req.body.pw;
+  console.log(req.body)
+  users.forEach(function(user) {
+    user.name=name;
+    user.pw=pw;    
+  });
+  res.redirect("/utilisateurs");
+});
+
+//delete user function
+app.get("/delete/:id",function(req,res){
+  var id=req.params.id;
+  console.log(id);
+  users=users.filter(function(user){
+    return (user.id != id);
+  });
+  res.redirect("/utilisateurs");
+});
+
+
+//add user route
+app.get("/utilisateur/add_utilisateur",function(req,res){
+  res.render("ajout");
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -37,5 +79,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(3000,()=>{
+  console.log("server ON");
+})
 
 module.exports = app;
